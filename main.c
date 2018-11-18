6ededa02899340cd8718f7db2a037ada9bfb5650
Stefan Tan <stan3@stuy.edu>
2018-11-18 00:40:55 -0500

    completed work15

--- main.c
+++ main.c
@@ -2,15 +2,37 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
 
 int main() {
-  prinf("Initial Message of the Parent Process!\n");
-  int cpid1 = fork();
 
-  if (fork() == 0)
-    printf("Child's pid: %d", getpid())
+  srand(time(NULL));
+  int num1 = rand() % 16 + 5;
+  int num2 = rand() % 16 + 5;
+  printf("Initial Message of the Parent Process! pid: %d\n", getpid());
 
-
+  if(fork()) { //parent
+    if(fork()) { //parent
+      int status; 
+      int cpid = wait(&status); 
+      printf("Child with the pid of %d was asleep for %d seconds.\n", cpid, WEXITSTATUS(status));
+      printf("Parent Process is Done!\n"); 
       return 0;
+    }
+
+    else { //child2
+      printf("Child 2's pid: %d\n", getpid());
+      sleep(num2); 
+      printf("Child 2 is done sleeping for %d seconds!\n", num2); 
+      return num2;
+    }
+  }
+
+  else { //child1
+    printf("Child 1's pid: %d\n", getpid());
+    sleep(num1); 
+    printf("Child 1 is done sleeping for %d seconds!\n", num1); 
+    return num1;
+  }
+}
   
-      }
